156ced26319ed1f60deb42fa341f05897632ab28
Eamon Caddigan <ecaddigan@evidation.com>
2021-04-14 16:25:19 -0400

    Removed magrittr dependency

--- analyses/team2/preprocess/ec_functions.R
+++ analyses/team2/preprocess/ec_functions.R
@@ -1,4 +1,4 @@
-
+# Functions to help with data preprocessing
 
 #' Clean the sentencing period data and split it into a minimum and maximum.
 #'
@@ -44,11 +44,10 @@ clean_dispositions_periods <- function(period, min_period, max_period) {
                                                     "\\.00", "")
   
   # Do basic cleanup
-  clean_periods <- clean_periods %>%
-    tolower() %>%
-    str_replace_all("[[:blank:]]+", " ") %>%
-    str_replace("^ ", "") %>%
-    str_replace_all(" $", "")
+  clean_periods <- tolower(clean_periods)
+  clean_periods <- str_replace_all(clean_periods, "[[:blank:]]+", " ")
+  clean_periods <- str_replace(clean_periods, "^ ", "")
+  clean_periods <- str_replace_all(clean_periods, " $", "")
   
   # FOR NOW we're going to throw out extra information about IPP
   clean_periods <- str_replace(clean_periods,
@@ -60,26 +59,23 @@ clean_dispositions_periods <- function(period, min_period, max_period) {
                                "life_no_parole")
   
   # Deal with the many ways of writing ".5"
-  clean_periods <- clean_periods %>%
-    str_replace_all("([[:digit:]]+) - 1/2", "\\1.5") %>%
-    str_replace_all("([[:digit:]]+) 1/2", "\\1.5") %>%
-    str_replace_all("([[:digit:]]+) and a half", "\\1.5") 
+  clean_periods <- str_replace_all(clean_periods, "([[:digit:]]+) - 1/2", "\\1.5")
+  clean_periods <- str_replace_all(clean_periods, "([[:digit:]]+) 1/2", "\\1.5")
+  clean_periods <- str_replace_all(clean_periods, "([[:digit:]]+) and a half", "\\1.5") 
   
   # Tokenize "time in"/"time-in", "time served" and some typos
-  clean_periods <- clean_periods %>%
-    str_replace("tiime", "time") %>%
-    str_replace("tim ", "time") %>%
-    str_replace("time[ -]?(served|in)", "time_served") %>%
-    str_replace("balance of backtime", "time_served") %>%
-    str_replace("back time", "time_served")
+  clean_periods <- str_replace(clean_periods, "tiime", "time")
+  clean_periods <- str_replace(clean_periods, "tim ", "time")
+  clean_periods <- str_replace(clean_periods, "time[ -]?(served|in)", "time_served")
+  clean_periods <- str_replace(clean_periods, "balance of backtime", "time_served")
+  clean_periods <- str_replace(clean_periods, "back time", "time_served")
   
   # Standardize units
-  clean_periods <- clean_periods %>%
-    str_replace_all("hrs", "hours") %>%
-    str_replace_all("hour(?!s)",  "hours") %>%
-    str_replace_all("day(?!s)",   "days") %>%
-    str_replace_all("month(?!s)", "months") %>%
-    str_replace_all("year(?!s)",  "years")
+  clean_periods <- str_replace_all(clean_periods, "hrs", "hours")
+  clean_periods <- str_replace_all(clean_periods, "hour(?!s)",  "hours")
+  clean_periods <- str_replace_all(clean_periods, "day(?!s)",   "days")
+  clean_periods <- str_replace_all(clean_periods, "month(?!s)", "months")
+  clean_periods <- str_replace_all(clean_periods, "year(?!s)",  "years")
   
   # Remove "flat"
   clean_periods <- str_replace(clean_periods, " flat", "")
@@ -88,10 +84,9 @@ clean_dispositions_periods <- function(period, min_period, max_period) {
   clean_periods <- str_replace(clean_periods, " bcp", "")
   
   # Standardize the "time arithmetic"
-  clean_periods <- clean_periods %>%
-    str_replace_all(", ", " plus ") %>%
-    str_replace_all(" and ", " plus ") %>%
-    str_replace_all(" less ", " minus ")
+  clean_periods <- str_replace_all(clean_periods, ", ", " plus ")
+  clean_periods <- str_replace_all(clean_periods, " and ", " plus ")
+  clean_periods <- str_replace_all(clean_periods, " less ", " minus ")
   
   # This one's tricky, in part because str_replace_all is greedier than it should
   # be. Turn entries like "2 years 6 months" into "2 years plus six months".
@@ -104,15 +99,14 @@ clean_dispositions_periods <- function(period, min_period, max_period) {
                                                             "\\1 \\2 plus \\3 \\4")
   
   # Split the periods using "-" or "to"
-  clean_periods_split <- clean_periods %>%
-    str_split_fixed("\\s?((\\bto\\b)|-)\\s?", 2)
+  clean_periods_split <- str_split_fixed(clean_periods, "\\s?((\\bto\\b)|-)\\s?", 2)
   
   # If the beginning period is missing a time unit (days, months, years), grab it from the end period
   is_missing_units <- str_detect(clean_periods_split[, 1], "^[[:digit:]]+(\\.5)?$")
   # Pulling out the first units word is a bit tricky
-  missing_units <- clean_periods_split[is_missing_units, 2] %>%
-    str_extract("^[[:digit:]]+(\\.5)? [[:alpha:]]+") %>%
-    str_extract("[[:alpha:]]+$")
+  missing_units <-  str_extract(str_extract(clean_periods_split[is_missing_units, 2], 
+                                            "^[[:digit:]]+(\\.5)? [[:alpha:]]+"), 
+                                "[[:alpha:]]+$")
   clean_periods_split[is_missing_units, 1] <- paste(clean_periods_split[is_missing_units, 1],
                                                     missing_units, sep = " ")
   
